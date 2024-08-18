0bfba838bac70f45407427ac6c8ed0bd28561604
Zakhar <zaharlazarev123@gmail.com>
2024-08-18 20:21:32 +0300

    all

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import clsx from "clsx";
+import css from './Description.module.css'
+export default function Description(){
+  return(
+  <div>
+    <h1 className={clsx(css.DescriptionHeader)}>Sip Happens Café</h1>
+    <p>Please leave your feedback about our service by selecting one of the options below.</p>
+  </div>
+  )
+}
